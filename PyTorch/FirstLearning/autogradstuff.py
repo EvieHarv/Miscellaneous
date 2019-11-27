b1501bc7678ecdae0a0a0891b0cd2e81e603fdf7
EthanHarv <github@ethanharv.com>
2019-11-27 14:56:06 -0600

    Learned some autograd, changed folder name

--- PyTorch/FirstLearning/autogradstuff.py
+++ PyTorch/FirstLearning/autogradstuff.py
@@ -22,22 +22,46 @@ out = z.mean()
 print(out)
 out.backward()
 print(x)
-print(x.grad)
+print(x.grad) # the orig. derivitive of d(o)/dx where o(x) = (1/4) ∑(i) z_i, z_i=3(x_i+2)^2
 
-x = torch.randn(3, requires_grad=True)
+h = torch.ones(2, 2, requires_grad=True) 
 
-y = x * 2
-while y.data.norm() < 1000:
-    y = y * 2
-print(y)
+i = h - 3
+j = i * i * 8
 
-v = torch.tensor([0.1, 1.0, 0.0001], dtype=torch.float)
-y.backward(v)
+k = j.mean()
+"""
+Alright, so if im right, it should be 
+k(h) = (1/4) ∑(i) 8(h_n - 3)^2
+which should differentiate to
+d(k)/d(h) = 4(h_n - 3)
+so for all h_n = 1, our final tensor output should be -8 in all slots?
+"""
 
-print(x.grad)
+k.backward()
+print(h)
+print(h.grad)
 
-print(x.requires_grad)
-print((x ** 2).requires_grad)
+"""
+WOOOOOO I WAS RIGHT!
+i know its not that hard and its only basic calculus but for some reason that .backward() algorithm took me an hour or two to wrap my head around.
+I'm really happy that that test worked the first time, that gives me confidence that I actually know what im doing lmao
+"""
 
-with torch.no_grad():
-    print((x ** 2).requires_grad)
+#x = torch.randn(3, requires_grad=True)
+#
+#y = x * 2
+#while y.data.norm() < 1000:
+#    y = y * 2
+#print(y)
+#
+#v = torch.tensor([0.1, 1.0, 0.0001], dtype=torch.float)
+#y.backward(v)
+
+#print(x.grad)
+#
+#print(x.requires_grad)
+#print((x ** 2).requires_grad)
+#
+#with torch.no_grad():
+#    print((x ** 2).r equires_grad)
