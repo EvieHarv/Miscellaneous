9578bac003d930220af826574804a7ba28837301
EthanHarv <github@ethanharv.com>
2019-12-11 19:35:10 -0600

    Created Maven Project

--- Plugins/hello.java
+++
@@ -1,9 +0,0 @@
-/**
- * hello
- */
-public class hello {
-
-    public static void main(String[] args) {
-        System.out.println("I dislike java immesely! I still gotta use it for this project though."); 
-    }
-}
